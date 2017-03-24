c25786dd1dacf31bf52ce69ca28efa3d647f21c4
Eben <me@ebenroux.co.za>
2017-03-24 07:29:06 +0200

    - v8.0.0

--- Shuttle.Esb.Sql.Subscription/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.Sql.Subscription/Properties/AssemblyInfo.cs
@@ -26,10 +26,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Esb.Sql.Subscription for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2017")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("8.0.0")]
 [assembly: ComVisible(false)]
