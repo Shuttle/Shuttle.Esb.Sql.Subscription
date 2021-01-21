473edbcd20380555f09cf00e66fc1451d14a4d7e
Eben <me@ebenroux.co.za>
2021-01-21 12:53:59 +0200

    - v11.0.4

--- Shuttle.Esb.Sql.Subscription.Tests/DataAccessFixture.cs
+++ Shuttle.Esb.Sql.Subscription.Tests/DataAccessFixture.cs
@@ -4,8 +4,9 @@ using Moq;
 using NUnit.Framework;
 using Shuttle.Core.Data;
 using Shuttle.Core.Transactions;
-#if (NETCOREAPP2_0 || NETCOREAPP2_1 || NETSTANDARD2_0)
-using Shuttle.Core.Data.SqlClient;
+#if NETCOREAPP
+using System.Data.Common;
+using System.Data.SqlClient;
 #endif
 
 namespace Shuttle.Esb.Sql.Subscription.Tests
@@ -16,6 +17,10 @@ namespace Shuttle.Esb.Sql.Subscription.Tests
         [OneTimeSetUp]
         public void GlobalSetup()
         {
+#if NETCOREAPP
+            DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance);
+#endif
+            
             var connectionConfigurationProvider = new Mock<IConnectionConfigurationProvider>();
 
             connectionConfigurationProvider.Setup(m => m.Get(It.IsAny<string>())).Returns(
@@ -25,19 +30,11 @@ namespace Shuttle.Esb.Sql.Subscription.Tests
                     "server=.\\sqlexpress;database=shuttle;Integrated Security=sspi;"));
 
 
-#if (!NETCOREAPP2_0 && !NETCOREAPP2_1 && !NETSTANDARD2_0)
             DatabaseContextFactory = new DatabaseContextFactory(
                 connectionConfigurationProvider.Object,
                 new DbConnectionFactory(), 
                 new DbCommandFactory(), 
                 new ThreadStaticDatabaseContextCache());
-#else
-            DatabaseContextFactory = new DatabaseContextFactory(
-                connectionConfigurationProvider.Object,
-                new DbConnectionFactory(new DbProviderFactories()),
-                new DbCommandFactory(),
-                new ThreadStaticDatabaseContextCache());
-#endif
             
             DatabaseGateway = new DatabaseGateway();
 
